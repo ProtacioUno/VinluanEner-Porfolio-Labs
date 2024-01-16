a6f605992d984da7fb09a92136d026859ed578c2
ProtacioUno <enervinluan01@gmail.com>
2024-01-16 21:23:23 +0800

    021

--- Python Project/MeasureOfPosition.py
+++ Python Project/MeasureOfPosition.py
@@ -1,1 +1,16 @@
-sampoinks
+
+
+DK_list = [3 ,4, 6]
+
+dataset = [1 ,2 , 3 ,7 , 9] 
+
+# print(len(dataset))
+
+def decile(i) :
+	answer = (i * (len(dataset) + 1)) / 10	
+	print(f"the decile one are : {answer}")
+	
+for x in range(len(DK_list)):
+	decile(DK_list[x])
+
+	
